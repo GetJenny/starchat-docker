0778aec99b308a44ea30dd1a2383b4f60ef2c4ca
Angelo Leto <angelo@getjenny.com>
2020-02-12 17:04:47 +0100

    removed old scripts

--- scripts/postDeleteAllDecisionTables.sh
+++
@@ -1,4 +0,0 @@
-PORT=${1:-8888}
-INDEX_NAME=${2:-index_getjenny_english_0}
-curl -v -H "Authorization: Basic $(echo -n 'admin:adminp4ssw0rd' | base64)" \
- -H "Content-Type: application/json" -X DELETE http://localhost:${PORT}/${INDEX_NAME}/decisiontable/all
--- scripts/runCmd.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-docker run -i -t --rm --name starchat elegansio/starchat:latest $@
-
